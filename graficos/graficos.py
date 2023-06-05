2b85c2206d945692d23451db8ae2dfdaff9b986f
Lautaro Bachmann <laurigbach@gmail.com>
2023-06-05 20:56:58 -0300

    Separar en otro archivo codigo para graficar utilizacion

--- graficos/graficos.py
+++ graficos/graficos.py
@@ -6,7 +6,6 @@ import pandas  # libreria para procesamiento de datos muy usada en la industria
 import matplotlib.pyplot as plt  # librería para graficas de python
 import json
 import seaborn  # Extiende las funcionalidades de matplotlib, muy usado en data visualization
-import numpy  # extiende las librerias de matemática de python
 
 # Con esto hacemos los gráficos más grandes, adecuados para el proyector.
 seaborn.set_context(context='talk', font_scale=1.2)
@@ -27,24 +26,9 @@ class DfWrapper:
             data = json.load(json_file)
         return data
 
-    def get_inter_arrival_time(self, item):
-        value = item['config'][4]["Network.node[{0,1,2,3,4,6,7}].app.interArrivalTime"]
-        return float(value.split("(")[-1][:-1])
-
-    def get_inter_arrival_times(self):
-        result = [
-            self.get_inter_arrival_time(row) for _, row in self.data.iterrows()]
-        print(result)
-        return result
-
-    def sort_json(self, data):
-        data = list(data)
-        return sorted(data, key=self.get_inter_arrival_time)
-
     def set_data_from_json(self):
         json_data = self.load_json_file(self.file_path).values()
-        sorted_data = self.sort_json(json_data)
-        self.data = pandas.DataFrame.from_dict(sorted_data)
+        self.data = pandas.DataFrame.from_dict(json_data)
 
     def get_val_from_col(self, col, key):
         result = []
@@ -66,29 +50,9 @@ def get_gen_intervals(data):
 
 
 def draw_delay(data, index, rindex):
-    gen_intervals, delays = get_gen_intervals(data), get_delays(data)
-    print(gen_intervals)
-    plt.plot(gen_intervals, delays, linewidth=0.5 + rindex*2,  zorder=index,
-             label=f"{data.file_path.split('.json')[0]}")
-
-
-def get_total_packets_by_key(data, key):
-    packets = data.get_val_from_col("scalars", key)
-    groups = [packets[i:i + NODE_AMMOUNT]
-              for i in range(0, len(packets), NODE_AMMOUNT)]
-    return [sum(group) for group in groups]
-
-
-def draw_carga(data, index, rindex):
-    paquetes_emisor = get_total_packets_by_key(data, "sentPackets")
-    paquetes_receptor = get_total_packets_by_key(data, "receivedPackets")
-    carga = [
-        received/sent for sent, received in zip(paquetes_emisor, paquetes_receptor)
-    ]
-    print(carga)
-    arriv_times = data.get_inter_arrival_times()
-    plt.plot(arriv_times, carga, linewidth=0.5 + rindex*2, zorder=index,
-             label=f"{data.file_path.split('.json')[0]}")
+    delay = data.get_val_from_col("scalars", "Network.node[5].app")
+    print(delay)
+    plt.bar(index, delay, label=f"{data.file_path.split('.json')[0]}")
 
 
 def draw_with_func(sources, draw_func):
@@ -106,22 +70,13 @@ def draw_delays(sources):
     draw_with_func(sources, draw_delay)
 
 
-def draw_cargas(sources):
-    plt.xlabel("interArrivalTime")
-    plt.ylabel("Received/Sent")
-    draw_with_func(sources, draw_carga)
-
-
 def main():
     plt.style.use("ggplot")
-    # caso1 = [
-    #    "tc1p1.json",
-    # ]
-    caso2 = [
-        "p1c2.json",
+    caso1 = [
+        "p1c1.json",
+        "p1c2_IAT8.json",
     ]
-    # draw_delays(caso1)
-    draw_cargas(caso2)
+    draw_delays(caso1)
 
 
 if __name__ == '__main__':
