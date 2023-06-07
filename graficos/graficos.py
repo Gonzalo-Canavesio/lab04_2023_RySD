61882680d7ee0664308a2a116a17dd4b864439d1
Lautaro Bachmann <laurigbach@gmail.com>
2023-06-07 19:40:01 -0300

    Crear funcion para graficar saltos y empezar a guardar imagenes

--- graficos/graficos.py
+++ graficos/graficos.py
@@ -47,35 +47,59 @@ class DfWrapper:
         return result
 
 
-def draw_delay(data, index, rindex):
-    delay = data.get_val_from_col_and_module(
-        "scalars", "Network.node[5].app", "Average delay")
-    print(delay)
-    plt.bar(index, delay, label=f"{data.file_path.split('.json')[0]}")
-
-
 def draw_with_func(sources, draw_func):
     source_length = len(sources)
     for index, source in enumerate(sources):
         data = DfWrapper(source)
         draw_func(data, index, source_length - index)
     plt.legend(loc='best')
-    plt.show()
+    figname = draw_func.__name__ + "-" + \
+        "-".join((source.split(".")[0] for source in sources))
+    print(f"Saving fig to {figname}.png")
+    plt.savefig(figname)
+    plt.close()
+
+
+def draw_delay(data, index, rindex):
+    delay = data.get_val_from_col_and_module(
+        "scalars", "Network.node[5].app", "Average delay")
+    name = f"{data.file_path.split('.json')[0]}"
+    plt.bar([name], delay, ec="black", lw=2)
 
 
 def draw_delays(sources):
-    plt.xlabel("generationInterval")
+    plt.title("Delays")
     plt.ylabel("Average Delay")
     draw_with_func(sources, draw_delay)
+
+
+def draw_hop(data, index, rindex):
+    hops = data.get_val_from_col_and_module(
+        "scalars", "Network.node[5].app", "hopCount")
+    name = f"{data.file_path.split('.json')[0]}"
+    plt.bar([name], hops, ec="black", lw=2)
+
+
+def draw_hops(sources):
+    plt.title("Saltos")
+    plt.ylabel("Average Hops")
+    draw_with_func(sources, draw_hop)
 
 
 def main():
     plt.style.use("ggplot")
     caso1 = [
         "p1c1.json",
+        "p2c1.json",
+    ]
+    caso2 = [
         "p1c2_IAT8.json",
+        "p2c2_IAT8.json",
     ]
     draw_delays(caso1)
+    draw_delays(caso2)
+    draw_hops(caso1)
+    draw_hops(caso2)
 
 
 if __name__ == '__main__':
